1847c182a3a4e46a78e80a97e4475447104d45b2
Prateek Varma <prtk.varma@gmail.com>
2022-12-22 16:41:03 -0600

    Setting up REST API routes, testable from POSTMAN

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,12 @@
 exports.getPosts = (req, res, next) => {
-    
+    res.status(200).json({ posts: [{ title: 'First Post' }  ] });
+};
+
+exports.createPost = (req, res, next) => {
+    const title = req.body.title;
+    const content = req.body.content;
+    res.status(201).json({
+        message: 'Post created successfully!',
+        post: { id: new Date().toISOString(), title: title, content: content }
+    });
 };
