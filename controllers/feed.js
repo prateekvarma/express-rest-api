7da82c9c96bdfa433c6b462248ed7d2e90530a63
Prateek Varma <prtk.varma@gmail.com>
2022-12-28 14:40:54 -0600

    Added more fields to reponse data, after creating a post

--- controllers/feed.js
+++ controllers/feed.js
@@ -20,6 +20,14 @@ exports.createPost = (req, res, next) => {
     const content = req.body.content;
     res.status(201).json({
         message: 'Post created successfully!',
-        post: { id: new Date().toISOString(), title: title, content: content }
+        post: { 
+            _id: new Date().toISOString(), 
+            title: title, 
+            content: content,
+            creator: {
+                name: 'Prateek'
+            },
+            createdAt: new Date()
+        }
     });
 };
