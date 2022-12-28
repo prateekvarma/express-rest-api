eb1f2f5d4c265fd4f7fdcdf0f00f2c6432c9b4e1
Prateek Varma <prtk.varma@gmail.com>
2022-12-28 14:08:32 -0600

    Added more fields to the GET post API endpoint

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,5 +1,18 @@
 exports.getPosts = (req, res, next) => {
-    res.status(200).json({ posts: [{ title: 'First Post' }  ] });
+    res.status(200).json({ 
+        posts: [
+            {
+                _id: '1',
+                title: 'First Post',
+                content: 'This is the first post content',
+                imageURL: 'images/puppy.jpg',
+                creator: {
+                    name: 'Prateek'
+                },
+                createdAt: new Date()
+            }  
+        ] 
+    });
 };
 
 exports.createPost = (req, res, next) => {
