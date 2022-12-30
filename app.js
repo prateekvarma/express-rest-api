196df44334a1e4d8b2f438309f481e0772f137a2
Prateek Varma <prtk.varma@gmail.com>
2022-12-30 14:54:32 -0600

    Installed mongoose, and setup a Post model configured to MongoDB Atlas service

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const feedRoutes = require('./routes/feed');
 
@@ -16,4 +17,6 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+mongoose.connect('xxx-mongoDB-URL-xxx').then((result) => {
+    app.listen(8080);
+}).catch(err => console.log('Got an error: ', err))
