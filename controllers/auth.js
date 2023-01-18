c50e13dae5cb2c6f7be162c0110c55373aa7e8bc
Prateek Varma <prtk.varma@gmail.com>
2023-01-18 16:41:34 -0600

    Installed 'jsonwebtoken' package, created a JWT on successful auth, and sent the token as response - working

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const { validationResult } = require('express-validator/check');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -62,7 +63,13 @@ exports.login = (req, res, next) => {
           throw error;
         }
         // reached here, the password should have matched
-        // **Create a JWT token**
+        // Create a JWT token
+        // *Never send raw password in the JWT signed object*
+        const token = jwt.sign({ 
+          email: loadedUser.email, 
+          userId: loadedUser._id.toString() 
+        }, 'secret');
+        res.status(200).json({ token: token, userId: loadedUser._id.toString() });
       })
       .catch((err) => {
         if(!err.statusCode) {
