8d51006e4f09f79bfcea9d69af4f414581cf4747
Prateek Varma <prtk.varma@gmail.com>
2023-01-18 16:19:22 -0600

    Created /login route, and to extract a user from the DB using the email

--- controllers/auth.js
+++ controllers/auth.js
@@ -37,3 +37,37 @@ exports.signup = (req, res, next) => {
   });
   
 };
+
+exports.login = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  let loadedUser;
+
+  User.findOne({ email: email }); //find the user from DB
+      .then((user) => {
+        if(!user) {
+          const error = new Error('Could not find user with that email');
+          error.statsCode = 401;
+          throw error;
+        }
+        //we found the user
+        loadedUser = user;
+        return bcrypt.compare(password, user.password);
+      })
+      .then((isEqual) => {
+        //isEqual is a promise returned by 'bcrypt.compare'
+        if(!isEqual) {
+          const error = new Error('Wrong password');
+          error.statusCode = 401;
+          throw error;
+        }
+        // reached here, the password should have matched
+        // **Create a JWT token**
+      })
+      .catch((err) => {
+        if(!err.statusCode) {
+          err.statusCode = 500; //if no errors yet, this error shold be a server error
+        }
+        next(err);
+      });
+};
